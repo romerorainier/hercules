7ce68ec8d934183260d83d78ced1432f36a9ac60
Rain <romero.rainier@gmail.com>
2025-10-31 22:44:47 +0800

    branch2

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-  allowedHosts: ['all'],
+  allowedHosts: ['devserver-main--vermillion-puffpuff-0d18b9.netlify.app'],
 },
 })
